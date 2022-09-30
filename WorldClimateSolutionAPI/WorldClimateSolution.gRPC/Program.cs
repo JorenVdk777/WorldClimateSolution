35b24244ad3112891a4852a8d4bbbd8d32dbc394
Robin Laevaert <robin.laevaert@axxes.com>
2022-09-30 12:30:31 +0200

    csv

--- WorldClimateSolutionAPI/WorldClimateSolution.gRPC/Program.cs
+++ WorldClimateSolutionAPI/WorldClimateSolution.gRPC/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using WorldClimateSolution.gRPC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +10,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+var test = GetCSV("https://github.com/JorenVdk777/WorldClimateSolution/blob/rlaevaert/BackEndAdded/WorldClimateSolutionAPI/WorldClimateSolution.gRPC/cities_air_quality_water_pollution.18-10-2021.csv").Split(',').Skip(1).Select(x => x);
+
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+
+string GetCSV(string url)
+{
+    HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+    HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+    StreamReader sr = new StreamReader(resp.GetResponseStream());
+    string results = sr.ReadToEnd();
+    sr.Close();
+
+    return results;
+} 
