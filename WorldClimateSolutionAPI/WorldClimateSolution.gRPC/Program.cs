7bea69cb9a95de3aec00b11459b798fd3f6db192
Robin Laevaert <robin.laevaert@axxes.com>
2022-09-30 13:02:50 +0200

    csv udate

--- WorldClimateSolutionAPI/WorldClimateSolution.gRPC/Program.cs
+++ WorldClimateSolutionAPI/WorldClimateSolution.gRPC/Program.cs
@@ -10,8 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc();
 
 var app = builder.Build();
-var test = GetCSV("https://github.com/JorenVdk777/WorldClimateSolution/blob/rlaevaert/BackEndAdded/WorldClimateSolutionAPI/WorldClimateSolution.gRPC/cities_air_quality_water_pollution.18-10-2021.csv").Split(',').Skip(1).Select(x => x);
+//var test = GetCSV("https://raw.githubusercontent.com/JorenVdk777/WorldClimateSolution/rlaevaert/BackEndAdded/WorldClimateSolutionAPI/WorldClimateSolution.gRPC/cities_air_quality_water_pollution.18-10-2021.csv").Split(',').Skip(1).Select(x => x).ToList();
+//var airpolWaterPol = test[456];
 
+
+WebClient webClient = new();
+var temp5 = Convert.ToDouble("4.5".Replace('.',','));
+webClient.DownloadFile("https://raw.githubusercontent.com/JorenVdk777/WorldClimateSolution/rlaevaert/BackEndAdded/WorldClimateSolutionAPI/WorldClimateSolution.gRPC/cities_air_quality_water_pollution.18-10-2021.csv", "airpolWaterPol");
+var airpolWaterPol = File.ReadAllLines("airpolWaterPol").Skip(1).Select(x => x.Split(',').ToList()).Select(x => new CityStats(){City = x[0], Country = x[2], Region=x[1], WaterQuality = Convert.ToDouble(x[4].Trim().Replace('.',',')), AirQuality = Convert.ToDouble(x[5].Trim().Replace('.',','))}).ToList();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
@@ -30,4 +36,20 @@ string GetCSV(string url)
     sr.Close();
 
     return results;
-} 
+}
+
+public class CityStatsOverview
+{
+    public List<CityStats> CityStats { get; set; }
+    public double AverageAirQuality { get; set; }
+    public double AverageWaterPollution { get; set; }
+}
+public class CityStats
+{
+    public string City { get; set; }
+    public string Region { get; set; }
+    public string Country { get; set; }
+    public double AirQuality { get; set; }
+    public double WaterQuality { get; set; }
+}
+
